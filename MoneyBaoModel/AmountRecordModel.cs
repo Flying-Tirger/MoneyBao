e66002a772350acf2429cf802cec68ce03f4d7f0
1667283631@qq.com <1667283631@qq.com>
2019-12-09 19:35:47 +0800

    写完

--- MoneyBaoModel/AmountRecordModel.cs
+++ MoneyBaoModel/AmountRecordModel.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace MoneyBaoModel
 {
-    class AmountRecordModel
+    
+    public class AmountRecordModel
     {
+        public int AmountId         { get; set; }
+        public string UserEmail	     { get; set; }
+        public double ApplyMoney	     { get; set; }
+        public double InvestorMoney    { get; set; }
+        public DateTime CreateTime	     { get; set; }
+        public int AmountState      { get; set; }
+        public int AmountDisthingId { get; set; }
+
     }
 }
--- MoneyBaoModel/ConventRecordModel.cs
+++ MoneyBaoModel/ConventRecordModel.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace MoneyBaoModel
 {
-    class ConventRecordModel
+    public class ConventRecordModel
     {
+        public int ConventRecordId  { get; set; }
+        public int ShopInfoId	     { get; set; }
+        public string UserEmail	     { get; set; }
+        public int ConventCount	 { get; set; }
+        public int ConsumeIntegral  { get; set; }
+        public DateTime ConventTime      { get; set; }
     }
 }
