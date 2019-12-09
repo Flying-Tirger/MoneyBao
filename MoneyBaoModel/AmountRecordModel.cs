2fe92f90fc6df321066bef8e8308117ebadf437f
1667283631@qq.com <1667283631@qq.com>
2019-12-09 19:40:58 +0800

    备注完全

--- MoneyBaoModel/AmountRecordModel.cs
+++ MoneyBaoModel/AmountRecordModel.cs
@@ -6,15 +6,38 @@ using System.Threading.Tasks;
 
 namespace MoneyBaoModel
 {
-    
+    /// <summary>
+    /// 项目记录表
+    /// </summary>
     public class AmountRecordModel
     {
+        /// <summary>
+        /// 项目表Id
+        /// </summary>
         public int AmountId         { get; set; }
+        /// <summary>
+        /// 用户邮箱
+        /// </summary>
         public string UserEmail	     { get; set; }
+        /// <summary>
+        /// 融资金额
+        /// </summary>
         public double ApplyMoney	     { get; set; }
+        /// <summary>
+        /// 投资金额
+        /// </summary>
         public double InvestorMoney    { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
         public DateTime CreateTime	     { get; set; }
+        /// <summary>
+        /// 状态(0:投资成功  1:筹资成功 2:正在审核 3:审核未通过 4:正在筹资)
+        /// </summary>
         public int AmountState      { get; set; }
+        /// <summary>
+        /// 区分项目(0:融资   1:投资)
+        /// </summary>
         public int AmountDisthingId { get; set; }
 
     }
