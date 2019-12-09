e0bc7a6cb7c688a755f7dae78013e0b5d607d88c
1829575905@qq.com <1829575905@qq.com>
2019-12-09 19:47:18 +0800

    杨圣宇修改

--- MoneyBaoModel/ShopInfoModel.cs
+++ MoneyBaoModel/ShopInfoModel.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace MoneyBaoModel
 {
-    class ShopInfoModel
+    public class ShopInfoModel
     {
+        ///商品Id
+        public int ShopInfoId { get; set; }
+        //商品名称
+        public string ShopName { get; set; }
+        //商品图片
+        public string ShopPicture { get; set; }
+        //商品价格
+        public double ShopMuch { get; set; }
+        //商品折扣
+        public double ShopDiscount { get; set; }
+        //库存量
+        public int StockCount { get; set; }
+        //商品状态(上架，下架)
+        public int ShopState { get; set; }
     }
 }
