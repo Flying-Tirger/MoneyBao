c2a87f7ebb8d16c18c3122d977c32ea355ac54e8
vv <vv@DESKTOP-BAG3R1Q>
2019-12-10 16:37:17 +0800

    吕星海修改了dal

--- MoneyBaoDAL/GrowthInfoDal.cs
+++ MoneyBaoDAL/GrowthInfoDal.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Dapper;
+using MoneyBaoModel;
+using System.Data.SqlClient;
 namespace MoneyBaoDAL
 {
     /// <summary>
@@ -11,6 +13,26 @@ namespace MoneyBaoDAL
     /// </summary>
     public class GrowthInfoDal
     {
+        SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=MoneyLeopard;Integrated Security=True");
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public int Post(GrowthInfoModel model)
+        {
+
+            string sql = $"insert into GrowthInfo values({model.GrowthId},{model.UserEmail},{model.ChangeTime},{model.BeforeIntegral},{model.AfterIntegra},{model.GrowthValueNow},{model.Cause},{model.DisthingId},{model.ChangeValue}";
+            return conn.Execute(sql);
+        }
+        /// <summary>
+        /// 显示
+        /// </summary>
+        /// <returns></returns> 
+        public List<GrowthInfoModel> Show()
+        {
+            return conn.Query<GrowthInfoModel>("select * from GrowthInfo join UserInfo on GrowthInfo.GrowthId=UserInfo.UserId join ConventRecord on UserInfo.UserId=ConventRecord.ConventRecordId join ShopInfo on ConventRecord.ConventRecordId=ShopInfo.ShopInfoId", conn).ToList();
+        }
 
     }
 }
