0d5df3f5d2fe211cb63cd609c0f6952ca8471584
vv <vv@DESKTOP-BAG3R1Q>
2019-12-17 09:40:53 +0800

    吕星海

--- MoneyBaoAPI/Controllers/GrowthInfoController.cs
+++ MoneyBaoAPI/Controllers/GrowthInfoController.cs
@@ -4,11 +4,29 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-
+using MoneyBaoBll;
+using MoneyBaoModel;
 namespace MoneyBaoAPI.Controllers
 {
     public class GrowthInfoController : ApiController
     {
-
+        GrowthInfoBll growthInfo = new GrowthInfoBll();
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public int Post(GrowthInfoModel model)
+        {
+            return growthInfo.Post(model);
+        }
+        /// <summary>
+        /// 显示
+        /// </summary>
+        /// <returns></returns> 
+        public List<GrowthInfoModel> Show(string UserEmail = null)
+        {
+            return growthInfo.Show(UserEmail);
+        }
     }
 }
