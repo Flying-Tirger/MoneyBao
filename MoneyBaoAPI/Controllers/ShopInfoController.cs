d4a026e241b876c67947baeb81d64f7cdb43749f
Zhangbao <huawei@DESKTOP-E4U3GTO>
2019-12-17 09:57:45 +0800

    ok

--- MoneyBaoAPI/Controllers/ShopInfoController.cs
+++ MoneyBaoAPI/Controllers/ShopInfoController.cs
@@ -14,11 +14,24 @@ namespace MoneyBaoAPI.Controllers
     
     public class ShopInfoController : ApiController
     {
+        /// <summary>
+        /// 实例化bll
+        /// </summary>
         ShopInfoBll bll = new ShopInfoBll();
+        /// <summary>
+        /// 商品显示
+        /// </summary>
+        /// <returns></returns>
         public List<ShopInfoModel> GetShopShow()
         {
             return bll.ShopShow();
         }
+        /// <summary>
+        /// 库存量修改
+        /// </summary>
+        /// <param name="ShopInfoId">商品Id</param>
+        /// <param name="Count">修改的库存量</param>
+        /// <returns></returns>
         public int Putshop(int ShopInfoId, int Count)
         {
             return bll.Update(ShopInfoId, Count);
